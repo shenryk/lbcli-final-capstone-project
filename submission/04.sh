7ea26fc8eb0e37237ea3b883b4d733f774b9c5b8
shenku <senhen123@gmail.com>
2026-07-30 20:11:46 +0300

    check script 4

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,2 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
-bitcoin-cli -signet getrawtransaction d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7 2 | jq '.vin[0]'
+bitcoin-cli getrawtransaction d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7 true | jq -r '.vin[0].txinwitness[1]'
