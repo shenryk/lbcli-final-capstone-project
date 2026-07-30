04bdcde00906d9c15409127adf0dd010cae00286
shenku <senhen123@gmail.com>
2026-07-30 20:13:49 +0300

    check script 6

--- submission/06.sh
+++ submission/06.sh
@@ -1,3 +1,3 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
-$ bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 243821) 2 | \
+bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 243821) 2 | \
   jq -r '.tx[] | select(any(.vin[]; .sequence < 4294967294)) | .txid'
